42c2b8a736f2a67057f85a69de82a284f53fbd0e
Rahul Garg <vihu89@gmail.com>
2019-10-06 11:12:41 -0700

    Test encode

--- crates/erldpc/src/lib.rs
+++ crates/erldpc/src/lib.rs
@@ -3,6 +3,7 @@ extern crate labrador_ldpc;
 extern crate rustler;
 
 use rustler::{Encoder, Env, Error, Term};
+use labrador_ldpc::LDPCCode;
 
 mod atoms {
     rustler::rustler_atoms! {
@@ -13,14 +14,18 @@ mod atoms {
 rustler::rustler_export_nifs!(
     "erldpc",
     [
-        ("add", 2, add)
+        ("encode", 1, encode)
     ],
     None
 );
 
-fn add<'a>(env: Env<'a>, args: &[Term<'a>]) -> Result<Term<'a>, Error> {
-    let a: i64 = args[0].decode()?;
-    let b: i64 = args[1].decode()?;
-
-    Ok((atoms::ok(), a + b).encode(env))
+fn encode<'a>(env: Env<'a>, args: &[Term<'a>]) -> Result<Term<'a>, Error> {
+    // Pick the TC128 code, n=128 k=64
+    // (that's 8 bytes of user data encoded into 16 bytes)
+    let code = LDPCCode::TC128;
+    let txdata: Vec<u8> = args[0].decode()?;
+    // Allocate memory for the encoded data
+    let mut txcode = vec![0u8; code.n()/8];
+    code.copy_encode(&txdata, &mut txcode);
+    Ok((atoms::ok()).encode(env))
 }
